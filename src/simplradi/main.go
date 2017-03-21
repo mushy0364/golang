2cc8c735d4ab2cfb77e7322cb33e94b23a6951d6
Joseph Mushyakov <jmush32456@gmail.com>
2017-03-20 21:15:06 -0400

    save

--- src/simplradi/main.go
+++ src/simplradi/main.go
@@ -5,4 +5,7 @@ import (
 	"os"
 )
 
-var perfect_squares []int = []int{1, 2, 3}
+var perfect_squares []int = []int{4,9,16,25,36,49,64,81,100,121,144,169,196,225,256,289,324,361,400]
+
+func main() {
+
